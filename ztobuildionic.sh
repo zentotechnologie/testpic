c76a5ed3e9eb5add3b7b01cc08d1b9b7ef2b8a2a
mnassih <mnassih@gmail.com>
2019-08-23 15:49:54 +0200

    INITIAL JENKINS FILE

--- ztobuildionic.sh
+++ ztobuildionic.sh
@@ -11,3 +11,9 @@
 
 echo "npm run ionic info"
 npm run ionic info
+
+echo "npm run ionic cordova platform add android@8.0.0"
+npm run ionic cordova platform add android@8.0.0 --debug --verbose
+
+echo "npm run ionic cordova requirements --debug --verbose"
+npm run ionic cordova requirements --debug --verbose
