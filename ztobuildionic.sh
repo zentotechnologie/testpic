0e141e7ecc1874489a338a0d73f0b16f23548e4f
mnassih <mnassih@gmail.com>
2019-08-24 00:41:17 +0200

    INITIAL JENKINS FILE

--- ztobuildionic.sh
+++ ztobuildionic.sh
@@ -4,15 +4,58 @@
  echo "Setup ANDROID_HOME"
  export ANDROID_HOME="./androidjdk/tools"
  export ANDROID_SDK_ROOT="./androidjdk/tools"
- export GRADLE_HOME="/var/lib/jenkins/workspace/readjenkinsfile/gradle"
- export GRADLE_USER_HOME="./gradle"
- export GRADLE_HOME_PATH="/var/lib/jenkins/workspace/readjenkinsfile/gradle/bin"
-
-
- export PATH=$PATH:$GRADLE_HOME_PATH
- echo $PATH
-
+ export PATH=$PATH:./gradle/bin
+ export GRADLE_HOME=./gradle
+ export GRADLE_USER_HOME=./gradle
  echo "GRADLE VERSION"
-gradle -v
+ gradle -v
+
+ echo "CLASSPATH ="
+ echo $CLASSPATH
+ export CLASSPATH=$CLASSPATH:$GRADLE_HOME/lib
+  echo "CLASSPATH AFTER="
+ echo $CLASSPATH
+
+ $ANDROID_HOME/bin/sdkmanager --install   "platforms;android-28"
+ yes | $ANDROID_HOME/bin/sdkmanager --licenses
+
+ $ANDROID_HOME/bin/sdkmanager --install   "platforms;android-26"
+ yes | $ANDROID_HOME/bin/sdkmanager --licenses
+
+  $ANDROID_HOME/bin/sdkmanager --install   "platforms;android-27"
+ yes | $ANDROID_HOME/bin/sdkmanager --licenses
+
+export PATH=$PATH:$ANDROID_HOME
+
+ echo "npm install"
+ npm install
+
+  echo "npm install cordova"
+ npm install cordova
+
+ echo "npm install ionic"
+ npm install ionic
+
+ echo "INSTALL CORDOVA RES"
+ npm i cordova-res
+
+
+
+
+echo "npm run ionic info"
+npm run ionic info
+
+echo "npm run ionic cordova platform add android@8.0.0"
+npm run ionic cordova platform add android@8.0.0 --debug --verbose
+
+echo "GENERATE RESSOURCES"
+
+npm run ionic cordova --force resources
+
+echo "npm run ionic cordova requirements --debug --verbose"
+npm run ionic cordova requirements --debug --verbose
+
+ echo "npm run ionic cordova build android --prod --debug --verbose"
+ npm run ionic cordova build android --prod --debug --verbose
 
 
