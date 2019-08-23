311a0f7ddeea76974e42a4035760f06817112e95
mnassih <mnassih@gmail.com>
2019-08-23 18:49:54 +0200

    INITIAL JENKINS FILE

--- ztobuildionic.sh
+++ ztobuildionic.sh
@@ -3,7 +3,7 @@
  java -version
  echo "Setup ANDROID_HOME"
  export ANDROID_HOME="./androidjdk/tools"
- $ANDROID_HOME/bin/sdkmanager --licenses "platform-tools" "platforms;android-28" "licenses"
+ $ANDROID_HOME/bin/sdkmanager --licenses --install "platforms;android-28"
 
  #echo "npm install"
  #npm install
