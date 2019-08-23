d3d688cc1c8f33c8abf6034fde997da553033512
mnassih <mnassih@gmail.com>
2019-08-23 19:46:07 +0200

    INITIAL JENKINS FILE

--- ztobuildionic.sh
+++ ztobuildionic.sh
@@ -3,6 +3,7 @@
  java -version
  echo "Setup ANDROID_HOME"
  export ANDROID_HOME="./androidjdk/tools"
+ export PATH=$PATH:./androidgraddle/graddle
  $ANDROID_HOME/bin/sdkmanager --install   "platforms;android-28"
 yes | $ANDROID_HOME/bin/sdkmanager --licenses
 
@@ -23,5 +24,4 @@ echo "npm run ionic cordova platform add android@8.0.0"
 npm run ionic cordova platform add android@8.0.0 --debug --verbose
 
 echo "npm run ionic cordova requirements --debug --verbose"
-echo $ANDROID_HOME
-#npm run ionic cordova requirements --debug --verbose
+npm run ionic cordova requirements --debug --verbose
