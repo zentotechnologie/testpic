71bd5d798f3fbb06f9af60352882c9272e7e3441
mnassih <mnassih@gmail.com>
2019-08-23 22:49:32 +0200

    INITIAL JENKINS FILE

--- ztobuildionic.sh
+++ ztobuildionic.sh
@@ -19,20 +19,20 @@
 export PATH=$PATH:$ANDROID_HOME
 
  echo "npm install"
-# npm install
+ npm install
 
  echo "npm install ionic"
-# npm install ionic
+ npm install ionic
 
   echo "npm install cordova"
-# npm install cordova
+ npm install cordova
 
 
 echo "npm run ionic info"
 npm run ionic info
 
 echo "npm run ionic cordova platform add android@8.0.0"
-#npm run ionic cordova platform add android@8.0.0 --debug --verbose
+npm run ionic cordova platform add android@8.0.0 --debug --verbose
 
 echo "npm run ionic cordova requirements --debug --verbose"
 npm run ionic cordova requirements --debug --verbose
