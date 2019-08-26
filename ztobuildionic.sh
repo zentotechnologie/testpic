990198941fa9521acf08172b6344972e8bc3e68f
mnassih <mnassih@gmail.com>
2019-08-26 11:35:31 +0200

    INITIAL JENKINS FILE

--- ztobuildionic.sh
+++ ztobuildionic.sh
@@ -8,5 +8,61 @@
 yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses
 sh $ANDROID_HOME/tools/bin/sdkmanager --verbose "platform-tools" "platforms;android-28"
 yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses
+
+wget "https://services.gradle.org/distributions/gradle-4.10.2-bin.zip"
+unzip gradle-4.10.2-bin.zip
+
+ export PATH=$PATH:gradle-4.10.2/bin
+ echo $PATH
+ export GRADLE_HOME=gradle-4.10.2
+ export GRADLE_USER_HOME=gradle-4.10.2
+ echo "GRADLE VERSION"
+ gradle -v
+
+$ANDROID_HOME/tools/bin/sdkmanager --install  "build-tools;26.0.1"
+ yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses
+
+$ANDROID_HOME/tools/bin/sdkmanager --install   "platforms;android-28"
+ yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses
+
+ $ANDROID_HOME/tools/bin/sdkmanager --install   "platforms;android-26"
+ yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses
+
+  $ANDROID_HOME/tools/bin/sdkmanager --install   "platforms;android-27"
+ yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses
+
+export PATH=$PATH:$ANDROID_HOME/tools:/var/lib/jenkins/workspace/readjenkinsfile/androidjdk/platforms:$ANDROID_HOME/platform-tools
+echo $PATH
+
+ echo "npm install"
+ npm install
+
+  echo "npm install cordova"
+ npm install cordova
+
+ echo "npm install ionic"
+ npm install ionic
+
+ echo "INSTALL CORDOVA RES"
+ npm i cordova-res
+
+echo "npm run ionic info"
+npm run ionic info
+
+echo "npm run ionic cordova platform add android@8.0.0"
+npm run ionic cordova platform add android@8.0.0 --debug --verbose
+
+echo "GENERATE RESSOURCES"
+
+npm run ionic cordova --force resources
+
+echo "npm run ionic cordova requirements --debug --verbose"
+npm run ionic cordova requirements --debug --verbose
+
+ echo "npm run ionic cordova build android --prod --debug --verbose"
+ npm run ionic cordova build android --prod --debug --verbose
+
+
+
 #$ANDROID_HOME/tools/bin/sdkmanager  tools --verbose
 
