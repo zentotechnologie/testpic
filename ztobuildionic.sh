151e33891570b3291b98a3f93c7b06d402f23af6
mnassih <mnassih@gmail.com>
2019-08-26 10:16:22 +0200

    INITIAL JENKINS FILE

--- ztobuildionic.sh
+++ ztobuildionic.sh
@@ -5,16 +5,17 @@
  export ANDROID_HOME="./androidjdk/tools"
  export ANDROID_SDK_ROOT="./androidjdk/tools"
  $ANDROID_HOME/bin/sdkmanager --install  "build-tools;26.0.1"
+  yes | $ANDROID_HOME/bin/sdkmanager --licenses
 
-wget "https://services.gradle.org/distributions/gradle-4.10.2-bin.zip"
-unzip gradle-4.10.2-bin.zip
+#wget "https://services.gradle.org/distributions/gradle-4.10.2-bin.zip"
+#unzip gradle-4.10.2-bin.zip
 
- export PATH=$PATH:gradle-4.10.2/bin
- echo $PATH
- export GRADLE_HOME=gradle-4.10.2
- export GRADLE_USER_HOME=gradle-4.10.2
- echo "GRADLE VERSION"
- gradle -v
+ #export PATH=$PATH:gradle-4.10.2/bin
+ #echo $PATH
+ #export GRADLE_HOME=gradle-4.10.2
+ #export GRADLE_USER_HOME=gradle-4.10.2
+ #echo "GRADLE VERSION"
+ #gradle -v
 
 
 
