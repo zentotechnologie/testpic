a56d665a1c6185319f90f4940effb2dea8a4cd43
mnassih <mnassih@gmail.com>
2019-08-26 09:30:53 +0200

    INITIAL JENKINS FILE

--- ztobuildionic.sh
+++ ztobuildionic.sh
@@ -4,9 +4,9 @@
  echo "Setup ANDROID_HOME"
  export ANDROID_HOME="./androidjdk/tools"
  export ANDROID_SDK_ROOT="./androidjdk/tools"
- export PATH=$PATH:./androidgraddle/gradle/bin
- export GRADLE_HOME=./androidgraddle/gradle
- export GRADLE_USER_HOME=./androidgraddle/gradle
+ export PATH=$PATH:./androidgraddle/gradle-4.10.2/bin
+ export GRADLE_HOME=./androidgraddle/gradle-4.10.2
+ export GRADLE_USER_HOME=./androidgraddle/gradle-4.10.2
  echo "GRADLE VERSION"
  gradle -v
 
