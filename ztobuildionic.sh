3fc6a406a3179058b3b2bdae879eeecd37d7cec2
mnassih <mnassih@gmail.com>
2019-08-26 11:29:25 +0200

    INITIAL JENKINS FILE

--- ztobuildionic.sh
+++ ztobuildionic.sh
@@ -5,6 +5,7 @@
  export ANDROID_HOME="/var/lib/jenkins/workspace/readjenkinsfile/androidjdk"
  export ANDROID_SDK_ROOT="/var/lib/jenkins/workspace/readjenkinsfile/androidjdk"
 
+yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses
 sh $ANDROID_HOME/tools/bin/sdkmanager --verbose "platform-tools" "platforms;android-28"
 yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses
 #$ANDROID_HOME/tools/bin/sdkmanager  tools --verbose
